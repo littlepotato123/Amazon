da49b572e43dcb00e5a7b6a59a012029533a3bfc
littlepotato123 <sopanda0910@gmail.com>
2021-12-01 19:35:43 -0500

    Setup

---
+++ api/src/index.ts
@@ -0,0 +1,25 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { HelloWorldResolver } from "./resolvers/HelloWorldResolver";
+
+(async () => {
+  const app = express();
+
+  await createConnection();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloWorldResolver]
+    }),
+    context: ({ req, res }) => ({ req, res })
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  app.listen(4000, () => {
+    console.log("http://localhost:4000/graphql");
+  });
+})();
